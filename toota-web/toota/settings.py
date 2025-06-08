d8559fdc9f2ca6d4066e429e6dc3cd110bbcc0b8
olalekan <lekanmojibola@gmail.com>
2025-06-08 02:20:41 +0100

    user integration

--- toota-web/toota/settings.py
+++ toota-web/toota/settings.py
@@ -167,10 +167,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Channels settings (single definition)
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [("127.0.0.1", 6379)],
-        },
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
 
